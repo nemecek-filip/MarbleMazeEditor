4ce745be193177c713c60449f63f05f2d37a471a
Rob Baldwin <rob@robbaldwin.net>
2019-04-30 10:00:20 +0100

    Add FileExporter Implementation

--- MarbleMazeEditor/Logic/FileExporter.swift
+++ MarbleMazeEditor/Logic/FileExporter.swift
@@ -9,9 +9,19 @@
 import Foundation
 
 class FileExporter {
-    func exportLevelToFile(levelString: String, filename: String? = "level.txt") -> URL? {
-        return nil
+    func exportLevelToFile(levelString: String, filename: String = "level.txt") -> URL? {
+        let fileURL = getDocumentsDirectory().appendingPathComponent(filename)
+        do {
+            try levelString.write(to: fileURL, atomically: true, encoding: .utf8)
+            return fileURL
+        } catch {
+            print("Failed to save file: \(error.localizedDescription)")
+            return nil
+        }
+    }
+    
+    private func getDocumentsDirectory() -> URL {
+        let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        return paths[0]
     }
 }
-
-
